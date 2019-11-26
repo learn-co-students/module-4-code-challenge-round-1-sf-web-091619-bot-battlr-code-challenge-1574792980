17e7eb937b72c3f0850b841d9102512f3ebb4972
kat-star <katherinewkwong@gmail.com>
2019-11-26 11:58:21 -0800

    Katherine complete code challenge 4

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -61,18 +61,14 @@ const BotSpecs = props => {
             <button
               className="ui button fluid"
               onClick={() =>
-                console.log('connect this to a function that shows all bots')
+                props.showAllBots()
               }
             >
               Go Back
             </button>
             <button
               className="ui button fluid"
-              onClick={() =>
-                console.log(
-                  "connect this to a function that adds this bot to your bot army list"
-                )
-              }
+              onClick={() => props.handleBotClick(bot)}
             >
               Enlist
             </button>
--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,16 +1,53 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from '../components/BotSpecs'
 
 class BotCollection extends React.Component {
-  //your code here
+	//your code here
+	state = {
+		showBotDetails: false,
+		bot: ""
+	}
+
+	updateBotDetails = (bot) => {
+		this.setState((prevState) => {
+			return {
+				showBotDetails: !prevState.showBotDetails,
+				bot: bot
+			}
+		})
+	}
+
+	showAllBots = () => {
+		this.setState({
+			showBotDetails: false
+		})
+	}
+
+	// handleClick = (e) => {
+	// 	this.props.updateSearchText(event.target.value)
+	// }
+	// onClick = { this.handleClick }
 
   render(){
+
+		const { allBots, addBotToArmy } = this.props
+
   	return (
+		<div>
+			<div>
+				<label htmlFor="name">Search By Bot Name: </label>
+					<input type="text" placeholder="name" id="name" ></input>
+				<button>Search</button>
+			</div>
   	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
+    		
+				{this.state.showBotDetails ? <div><BotSpecs bot={this.state.bot} handleBotClick={addBotToArmy} showAllBots={this.showAllBots} /></div> : <div className="row">{allBots.map(bot =>
+						<BotCard key={bot.id} bot={bot} handleBotClick={this.updateBotDetails} />
+						)}</div>
+				}
+
+				</div>
   	  </div>
   	);
   }
