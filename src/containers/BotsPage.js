1f0aaff9ed7f4f44092ebfcc732c7aab8f1e48a9
github username <github email address>
2019-11-26 12:00:41 -0800

    added step 4 improve

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BotCollection from "./BotCollection";
 import BotArmy from "./YourBotArmy"
+import BotSpec from "../components/BotSpecs"
 
 class BotsPage extends React.Component {
   state = {
     bots: [],
-    botsArmy: []
+    botsArmy: [],
+    clicked: false,
+    clickedBot: {}
   }
 
   componentDidMount() {
@@ -20,10 +23,18 @@ class BotsPage extends React.Component {
       }))
   }
 
+  handleViewClick = (bot) => {
+    this.setState({
+      clicked: !this.state.clicked,
+      clickedBot: bot
+    })
+  }
+
   handleClicksBotsArmy = (botToAdd) => {
     if (!this.state.botsArmy.includes(botToAdd)) {
       this.setState({
-        botsArmy: [...this.state.botsArmy, botToAdd]
+        botsArmy: [...this.state.botsArmy, botToAdd],
+        clicked: !this.state.clicked
       })
     }
   }
@@ -31,17 +42,24 @@ class BotsPage extends React.Component {
   handleClickRemoveBot = (botToRemove) => {
     let newBotsArmy = this.state.botsArmy.filter( bot => bot.id !== botToRemove.id )
     this.setState({
-      botsArmy: newBotsArmy
+      botsArmy: newBotsArmy,
+      clicked: false
+    })
+  }
+
+  handleGoBack = () => {
+    this.setState({
+      clicked: !this.state.clicked
     })
   }
 
   render() {
     
-    const {bots, botsArmy} = this.state
+    const {bots, botsArmy, clickedBot} = this.state
     return (
       <div>
         <BotArmy bots={botsArmy} handleClick={this.handleClickRemoveBot} />
-        <BotCollection bots={bots} handleClick={this.handleClicksBotsArmy} />
+        {!this.state.clicked ? (<BotCollection bots={bots} handleClick={this.handleViewClick} />) : (<BotSpec handleAdd={this.handleClicksBotsArmy} handleRemove={this.handleClickRemoveBot} bot={clickedBot} botsArmy={botsArmy} handleGoBack={this.handleGoBack}/>)}
       </div>
     );
   }
